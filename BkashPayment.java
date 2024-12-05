a9e95d5242ef47f8c914fe1553dcd416dd8c9ed1
Siam2910 <abid.siam@northsouth.edu>
2024-12-05 23:25:11 +0600

    Abid

---
+++ BkashPayment.java
@@ -0,0 +1,7 @@
+package travelbookingsystem;
+
+class BkashPayment {
+    void makePayment(double amount) {
+        System.out.println("Payment of BDT" + amount + " processed through Bkash.");
+    }
+}
